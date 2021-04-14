9d84d75039b75dd3b8a115cf3089c5d1b82276fe
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-14 15:53:57 -0400

    fdirs store str(path)

--- mm_data/__init__.py
+++ mm_data/__init__.py
@@ -5,10 +5,10 @@ from . import forcefields
 from . import trajectories
 
 _mol_path = Path(molecules.__file__)
-mols = {mol.name: mol for mol in _mol_path.parent.glob("*") if mol.is_file}
+mols = {mol.name: str(mol) for mol in _mol_path.parent.glob("*") if mol.is_file}
 
 _ff_path = Path(forcefields.__file__)
-ffs = {ff.name: ff for ff in _ff_path.parent.glob("*") if ff.is_file}
+ffs = {ff.name: str(ff) for ff in _ff_path.parent.glob("*") if ff.is_file}
 
 _traj_path = Path(trajectories.__file__)
-trajs = {traj.name: traj for traj in _traj_path.parent.glob("*") if traj.is_file}
+trajs = {traj.name: str(traj) for traj in _traj_path.parent.glob("*") if traj.is_file}
