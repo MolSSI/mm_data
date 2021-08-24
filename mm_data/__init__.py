1bd301a42a275acb9678fd5cf61ef3f3c16bafd8
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-08-24 00:08:50 -0400

    fix is_file() call and ignore certain file exts

--- mm_data/__init__.py
+++ mm_data/__init__.py
@@ -4,11 +4,25 @@ from . import molecules
 from . import forcefields
 from . import trajectories
 
+exts = ("__", ".pyc", ".py", "_offsets.npz")
+
 _mol_path = Path(molecules.__file__)
-mols = {mol.name: str(mol) for mol in _mol_path.parent.glob("*") if mol.is_file}
+mols = {
+    mol.name: str(mol)
+    for mol in _mol_path.parent.glob("*")
+    if (mol.is_file() and not mol.name.endswith(exts))
+}
 
 _ff_path = Path(forcefields.__file__)
-ffs = {ff.name: str(ff) for ff in _ff_path.parent.glob("*") if ff.is_file}
+ffs = {
+    ff.name: str(ff)
+    for ff in _ff_path.parent.glob("*")
+    if (ff.is_file() and not ff.name.endswith(exts))
+}
 
 _traj_path = Path(trajectories.__file__)
-trajs = {traj.name: str(traj) for traj in _traj_path.parent.glob("*") if traj.is_file}
+trajs = {
+    traj.name: str(traj)
+    for traj in _traj_path.parent.glob("*")
+    if (traj.is_file() and not traj.name.endswith(exts))
+}
